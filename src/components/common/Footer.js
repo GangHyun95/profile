2c5cbc6e98e2151e4707a13160d0e50ad6c775ae
hyun <hgh6128@gmail.com>
2023-02-07 12:12:21 +0900

    카카오 지도

---
+++ src/components/common/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="inner">
+        <p>2023 Portfolio By Jo Jun Young. &copy All Right Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/sub/Community.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import Layout from "../common/Layout";
+
+const Community = () => {
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  return <Layout title={"Community"}>Community</Layout>;
+};
+
+export default Community;
